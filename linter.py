efa364284b9e4ef567d99ff1c500049c93ad2895
Rose <rose@lain.org.uk>
2019-11-07 21:41:16 +0000

    strip a lot of rubbish out

--- linter.py
+++ linter.py
@@ -1,6 +1,9 @@
 from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
 import sys
+import re
 
+# for best results, invoke ghcid like this
+# ghcid --command="stack ghci --ghc-options \"-fno-diagnostics-show-caret\"" --outputfile=/tmp/ghcid.json
 class Ghcid(Linter):
     regex = r'\"start\":\[(?P<line>\d+),(?P<col>\d+)\],.*\"message\":\"(?P<message>.*)\"'
     multiline = True
@@ -16,4 +19,6 @@ class Ghcid(Linter):
 
     def split_match(self, match):
       match, line, col, error, warning, message, near = super().split_match(match)
+      message = re.sub(r"\\u\d+b\[;?\d+m|\\n", "", message) # strip out color codes
+      message = re.sub(r"\s{2,}", " ", message) # strip out excess whitespace
       return match, line, col, error, warning, message, near  
